d53a766aab6f89e1247ecc1d76a420076167ddf9
Timmy <oyinloyematthew6@gmail.com>
2025-03-04 00:07:21 +0100

    added new components

---
+++ src/Components/Organization/Organization.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {useState, useEffect} from 'react';
+import './Organization.css'
+
+
+const Organization = () => {
+
+
+    return (
+
+        <div className='organization-container'>
+
+            <div className="organization-head">
+                <h1 className="head">
+                    30+ Big Organization Makes 
+                    Candela their Event Lighting Brand
+                </h1>
+                <p className="text">Many Organization have collaborated with 
+                    Candela and they trust us with their Event Lighting</p>
+            </div>
+
+            <div className="organization-png">
+                
+            </div>
+        </div>
+    )
+}
+
+export default Organization;
