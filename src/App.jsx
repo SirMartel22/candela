f8318e36ce0acbf740150bc4b721b90d9604431d
Timmy <oyinloyematthew6@gmail.com>
2025-04-01 15:09:01 +0100

    working on the testimonial section

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,14 @@ import About from './Components/About/About'
 import Gallery from './Components/Gallery/Gallery'
 import Testimonials from './Components/Testimonials/Testimonials'
 import Video from './Components/Video/Video'
-// import Organization from './Components/Organization/Organization'
 import Contact from './Components/Contact/Contact';
 import Join from './Components/Join/Join';
 import Footer from './Components/Footer/Footer';
 import Testing from './Components/Test/Testing'
-// import NewTestimony from './Components/NewTestimony/NewTestimony'
-import NewTestimony from './Components/Testimonials/NewTestimony';
-
-// import { images } from './Components/Slider/Slider.json'
 
 import { Slider } from './Components/Slider/Slider'
+
+import { Testifiers } from './Components/Testimonials/Testimonials.json'
 
 
 const App = () =>{
@@ -26,17 +23,12 @@ const App = () =>{
       <Navbar />
       <Hero />
       <About />
-
-      {/* <Slider imageSlider= { images } /> */}
       <Slider  />
-
-      {/* <Gallery /> */}
       <Video />
-      {/* <Organization /> */}
       <Testing />
       <Join />
-      {/* <Testimonials /> */}
-      <NewTestimony />
+      <Testimonials Testifiers = { Testifiers } />
+
       <Contact />
       <Footer />
     </div>
--- src/Components/Testimonials/Testimonials.jsx
+++ src/Components/Testimonials/Testimonials.jsx
@@ -1,102 +1,54 @@
 import React, {useState, useEffect} from 'react'
-import './Testimonials.css'
-import {Link as ScrollLink} from 'react-scroll'
+// import { Testifiers } from './Testimonials.json'
+import classes from './Testimonials.module.css'
+
+import { Link as ScrollLink } from 'react-scroll'
+import { IoIosArrowForward, IoIosArrowBack } from "react-icons/io";
 
 
-const Testimonials = () => {
 
-        //getting a iterable testimonials instead of hardcoding it
-        // inside the testimonials return statement
-    const testimonials = [
-        {
-            name: "Maria Gonzalez",
-            post: "Theater Director",
-            testimony: `"Candela's lighting solutions transformed our theater production. The audience was completely immersed in every scene"`
-        },
+const Testimonials = ( {Testifiers} ) => {
 
-        {
-            name: "Jake Wilson",
-            post: "Production Designer",
-            testimony: `"Candela's DMX systems are incredibly intuitive and have revolutionized our light show. Setup time is cut in half, and the impact is twice as powerful."`
-        },
 
-        {
-            name: "Robert James",
-            post: "Theater Director",
-            testimony: `"The moment we switched to Candela for our dance studio, both instructors and students noticed the difference."`
-        },
-
-        {
-            name: "Amelia Taylor",
-            post: "Community Arts Director",
-            testimony: `"When our community theater upgraded to Candela, we weren't just getting better lights—we were getting a partner."`
-        },
-    ]
-
-    const [currentIndex, setCurrentIndex] = useState(0);
-
-    useEffect(() => {
-        const timer = setInterval(() => {
-            setCurrentIndex((prevIndex) => prevIndex === testimonials.length - 1 ? 0 : prevIndex + 1); 
-        }, 7000); //Slide every 5 seconds
-
-        return () => clearInterval(timer); //Cleanup on unmount
-    }, [testimonials.length]);
-
-    console.log("testimonial here " + testimonials);
-
-    const testimonies = testimonials.map((testimony, index) => {
-        console.log(testimony)
-        return (
-
-            <div
-                key={index} 
-                className={`testimony-card ${index === (currentIndex + 1) % testimonials.length? 'middle-testimony' : ''}`}
-            >
-                <p>{testimony.testimony}</p>
-                <h4>{testimony.name}</h4>
-                <h6>{testimony.post}</h6>
-            </div>
-        )
-        
-    })
-    console.log("after using .map() method " + testimonies);
     return (
+        <div className={classes.TestimonialContainer}>
 
-        <div className="testimonials-section">
-            <div className="testimonials-description">
-                <h1>What Sets Us Apart</h1>
-                <p>At Candela, we don't just illuminate stages—we transform spaces into experiences. 
-                    Our lights breathe life into every performance, creating moods that captivate audiences and elevate artists. 
-                    From warm intimate glows to powerful dynamic beams, 
-                    we craft the perfect ambience that makes each moment unforgettable. 
-                    Lighting isn't just what we do—it's who we are.
-                </p>
+             <div className={classes.TestimonialDescription}>
+                 <h1>What Sets Us Apart</h1>
+                 <p>At Candela, we don't just illuminate stages—we transform spaces into experiences. 
+                     Our lights breathe life into every performance, creating moods that captivate audiences and elevate artists. 
+                     From warm intimate glows to powerful dynamic beams, 
+                     we craft the perfect ambience that makes each moment unforgettable. 
+                     Lighting isn't just what we do—it's who we are.
+                 </p>
 
-                    <button className="testimonial btn">
-                        <ScrollLink to='contact' spy={true} smooth={true} duration={500}> 
-                            Become our VVIP
-                            </ScrollLink>
-                    </button>
-             </div>
+                 <button className="testimonial btn">
+                     <ScrollLink to='contact' spy={true} smooth={true} duration={500}> 
+                         Become our VVIP
+                         </ScrollLink>
+                 </button>
+                </div>
 
-            <div className="customer-testimonials">
-                <div className="testimonials-slider" style = {{
-                    transform: `translateY(-${currentIndex * 33.33}%)`,
-                    transition: 'transform 0.5s ease-in-out'
-                    }}>
+                <div className={classes.TestimonialSection}>
+                <IoIosArrowBack className/>
 
-                    {testimonies}
+                    {Testifiers.map((item, id)=>{
+                        return(
+                            <div className={classes.TestimonialCard}>
+                                <p>{item.description}</p>
+                                <h4>{item.name}</h4>
+                                <h5>{item.position}</h5>
+                            </div>
+                        )
+                    })}
+                    
+                <IoIosArrowForward />
 
                 </div>
-               
-            </div>
-           
+
+       
         </div>
-
     )
-
 }
 
-
-export default Testimonials
+export default Testimonials;
