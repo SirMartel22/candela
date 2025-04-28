9c8fa9f8c0b399c5c9d0ecc3b88076e52144d64c
Timmy <oyinloyematthew6@gmail.com>
2025-04-28 14:14:20 +0100

    remove the bg video and replace it with cloudinary link

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react'
-import HeroVideo from '../../assets/candelabgvideo.mp4'
 import './Hero.css'
 import { Link as ScrollLink } from 'react-scroll'
 import gsap from 'gsap';
@@ -32,7 +31,7 @@ const Hero = () => {
   return (
 
     <div className='hero-container' id='hero'>
-        <video src={HeroVideo} autoPlay muted loop>
+        <video src='https://res.cloudinary.com/djhfy0pr4/video/upload/v1745845871/candelabgvideo_z4dybk.mp4' autoPlay muted loop>
             
         </video>
         <div className= 'hero-text'>
